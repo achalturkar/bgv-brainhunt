6089ba788895a54341b521f9c5190c3df608bbc7
Achal <achalyturkar@gmail.com>
2026-06-23 17:59:33 +0530

    configuration change to static

--- app/sitemap.js
+++
@@ -1,6 +0,0 @@
-export default function sitemap() {
-  const base = 'https://bgv.brainhuntventures.com'
-  const routes = ['', '/about', '/products', '/services', '/industries', '/careers', '/contact']
-  const now = new Date()
-  return routes.map(r => ({ url: `${base}${r}`, lastModified: now, changeFrequency: 'monthly', priority: r === '' ? 1 : 0.8 }))
-}
---
+++ public/sitemap.js
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://bgv.brainhuntventures.com/</loc>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>https://bgv.brainhuntventures.com/about/</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://bgv.brainhuntventures.com/products/</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://bgv.brainhuntventures.com/services/</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://bgv.brainhuntventures.com/industries/</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://bgv.brainhuntventures.com/careers/</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://bgv.brainhuntventures.com/contact/</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>
